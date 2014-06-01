307a8299e0fb32e24892a0ea392a320d348ccf8e
David Kolossa <david.kolossa@googlemail.com>
2014-06-02 00:13:42 +0200

    Corrected scoping issues
    * Made sure that the function pointer to memset() may only be used from
      within explicit_bzero().

--- Linux/src/util.c
+++ Linux/src/util.c
@@ -3,10 +3,10 @@
 // explicit_bzero taken from NetBSD
 // The compiler will not optimize a call to a volatile function pointer
 // so we use this function to clean up sensitive memory areas.
-void *(*volatile memset_impl)(void *, int, size_t) = memset;
 
 void explicit_bzero(void *b, size_t len)
 {
+    static void *(*volatile memset_impl)(void *, int, size_t) = memset;
     (*memset_impl)(b, 0, len);
 }
 
