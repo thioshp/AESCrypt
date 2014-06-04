513510c9aafb99b36214d4a1583813df29eb2d21
David Kolossa <david.kolossa@googlemail.com>
2014-06-04 17:15:51 +0200

    Added copyright header to util.c

--- Linux/src/util.c
+++ Linux/src/util.c
@@ -1,6 +1,23 @@
+/*
+ * util.c
+ *
+ * Copyright (C) 2014
+ *
+ * This software is licensed as "freeware."  Permission to distribute
+ * this software in source and binary forms is hereby granted without a
+ * fee.  THIS SOFTWARE IS PROVIDED 'AS IS' AND WITHOUT ANY EXPRESSED OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * THE AUTHOR SHALL NOT BE HELD LIABLE FOR ANY DAMAGES RESULTING FROM
+ * THE USE OF THIS SOFTWARE, EITHER DIRECTLY OR INDIRECTLY, INCLUDING,
+ * BUT NOT LIMITED TO, LOSS OF DATA OR DATA BEING RENDERED INACCURATE.
+ *
+ */
+
 #include <string.h>
 
-// explicit_bzero taken from NetBSD
+// explicit_bzero taken from NetBSD, modified to be a pure wrapper to
+// memset()
 // The compiler will not optimize a call to a volatile function pointer
 // so we use this function to clean up sensitive memory areas.
 
